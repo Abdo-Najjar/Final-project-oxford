01310a3cc6992588bf72494d792a755cb87ce259
Abdo Najjar <raf7awe.overlord@gmail.com>
2020-06-06 23:34:29 +0300

    add application seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(CourseSeeder::class);
         $this->call(AdvertisementSeeder::class);
+        $this->call(ApplicationSeeder::class);
     }
 }
