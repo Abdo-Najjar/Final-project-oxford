144f9fd5f561e647b73cd5a1f857b04d161132c0
Abdo Najjar <raf7awe.overlord@gmail.com>
2020-08-03 18:45:20 +0300

    add relation between student and section

---
+++ database/migrations/2020_08_02_182537_create_section_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSectionUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('section_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->index();
+            $table->foreignId('section_id')->index();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('section_id')->references('id')->on('sections')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('section_user');
+    }
+}
