84a272551e6836519514ec7870f181af2c56b169
Abdo Najjar <raf7awe.overlord@gmail.com>
2020-08-04 19:57:28 +0300

    fix phone number faker

--- database/factories/ApplicationFactory.php
+++ database/factories/ApplicationFactory.php
@@ -4,6 +4,7 @@
 
 use App\Application;
 use App\CourseType;
+use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 $factory->define(Application::class, function (Faker $faker) {
@@ -15,10 +16,10 @@ $factory->define(Application::class, function (Faker $faker) {
         'email' => $faker->safeEmail,
         'address' => $faker->word(),
         'dob' => $faker->date(),
-        'phone_number' => $faker->phoneNumber,
+        'phone_number' => "059".random_int(0000000,9999999),
         'course_type_id' => $faker->randomElement($courseTypes) ,
         'days' => $faker->word(),
-        'time' => $faker->time,
+        'time' =>  Carbon::make($faker->time)->format('H:i') ,
         'major_of_study' => $faker->word(),
         'recognize' => $faker->sentence(50),
         'notes' => $faker->sentence(50),
