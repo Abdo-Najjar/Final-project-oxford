ca117a5354a1d9ac0c385169a5de9f7139d9399c
Abdo Najjar <raf7awe.overlord@gmail.com>
2020-08-04 20:37:22 +0300

    add type to section resource

--- app/Http/Resources/SectionResource.php
+++ app/Http/Resources/SectionResource.php
@@ -14,6 +14,17 @@ class SectionResource extends JsonResource
      */
     public function toArray($request)
     {
+
+        return [
+            'id'=>$this->id,
+            'name'=>$this->name,
+            'type'=>$this->course->courseType->name ,
+            'course_id'=>$this->course_id,
+            'user_id'=>$this->user_id,
+            'start_at'=>$this->start_at,
+            'end_at'=>$this->end_at
+        ];
+
         return parent::toArray($request);
     }
 }
